ccd7a3311961f4367050043d7c11ba1fe10e432c
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-10 17:48:56 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,8 +7,9 @@ def reformat_languages(languages)
     lang_hashes.each do |lang_names, attributes|
       if new_hash[lang_hashes] == nil || new_hash.keys.include?(lang_names) 
          new_hash[lang_names][:style] = just_styles
-        end
       end
+    end
+  end
   new_hash
 end
 
