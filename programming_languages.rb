c3f36f79cdafba944a93b63d23f648ee1c8073d5
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-10 15:46:36 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,10 +10,10 @@ def reformat_languages(languages)
      
      
 
-      if new_hash[lang_hashes] == nil || new_hash.keys.include?(lang_names) == false && lang_hashes.include?(lang_names) just_styles << styles
+      if new_hash[lang_hashes] == nil || new_hash.keys.include?(lang_names) == false && lang_hashes.include?(lang_names)
         new_hash[lang_names] = {}
         new_hash[lang_names] = attributes
-        new_hash[lang_names][:style] = just_styles
+        new_hash[lang_names][:style] = styles
         binding.pry
     
       end
